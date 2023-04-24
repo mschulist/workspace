6e437bfe2fead2d39dbc6cd4aff955d91044acd7
Mark Schulist <mschulist2@gmail.com>
2023-04-24 12:31:25 -0700

    more maniming

--- math_night/slides.py
+++ math_night/slides.py
@@ -7,47 +7,55 @@ class intro(Slide):
         title = Text("A Journey Around an Asteroid Belt")
         self.add(title)
         self.play(Write(title))
+        self.wait()
         self.next_slide()
 
         self.play(Unwrite(title))
-        problem = Text("Imagine you are exploring an asteroid belt, but you can only see the adjacent asteroids.", font_size=16)
+        problem = Text("Imagine you are exploring an asteroid belt, but you can only see the adjacent asteroids.", font_size=24)
         self.add(problem)
         self.play(Write(problem))
+        self.wait()
         self.next_slide()
 
         self.play(problem.animate.to_edge(UP))
-        specific_problem = Text("The number of days you spend on each asteroid must be proportional to the relative size of the asteroid.", font_size=16)
+        specific_problem = Text("The number of days you spend on each asteroid must be proportional to the relative size of the asteroid.", font_size=24)
         self.add(specific_problem)
         self.play(Write(specific_problem))
+        self.wait()
         self.next_slide()
 
         self.play(specific_problem.animate.next_to(problem, DOWN))
         question = Text("We need to develop a method to explore our asteroids\nwithout being able to see all of them at once.", font_size=32)
         self.add(question)
         self.play(Write(question))
+        self.wait()
         self.next_slide()
 
         self.play(Unwrite(problem), Unwrite(specific_problem), Unwrite(question))
-        answer = Text("We can use the Metropolis Algorithm to explore our asteroid belt!", font_size=24)
+        answer = Text("We can use the Metropolis Algorithm to explore our asteroid belt!", font_size=32)
         self.add(answer)
         self.play(Write(answer))
+        self.wait()
         self.next_slide()
 
         self.play(Unwrite(answer))
         rule1 = Text("Step 1: Start at a random asteroid", font_size=24).to_edge(UP).to_edge(RIGHT)
         self.add(rule1)
         self.play(Write(rule1))
+        self.wait()
         self.next_slide()
 
         asteroids = ImageMobject("asteroids_a.png").scale(0.7).to_edge(LEFT)
         person = ImageMobject("asteroids_person.png").scale(0.7).to_edge(LEFT)
         self.add(asteroids, person)
         self.play(FadeIn(asteroids), FadeIn(person))
+        self.wait()
         self.next_slide()
 
         rule2 = Text("Step 2: Flip a fair coin p(Heads) = 0.5 to\ndecide whether to propose going\nclockwise or counterclockwise.", font_size=24).next_to(rule1, DOWN).to_edge(RIGHT)
         self.add(rule2)
         self.play(Write(rule2))
+        self.wait()
         self.next_slide()
 
         coinCircle = Circle(radius=0.5, color=RED, fill_opacity=1)
@@ -55,6 +63,7 @@ class intro(Slide):
         Heads = Text("H", font_size=36).move_to(coinCircle.get_center())
         self.add(coinCircle, Heads)
         self.play(Create(coinCircle))
+        self.wait()
         self.next_slide()
 
 
@@ -62,16 +71,19 @@ class intro(Slide):
         clockwise_arrow = Arrow([-4,-1, 0], [-6, 1 ,0], color=WHITE)
         self.add(clockwise, clockwise_arrow)
         self.play(Write(clockwise), Create(clockwise_arrow))
+        self.wait()
         self.next_slide()
 
         rule3 = MathTex("\\text{Step 3: Flip a weighted coin, with p(heads)} = \\dfrac{\\text{proposal}}{\\text{current}}", font_size=28).next_to(coinCircle, DOWN).to_edge(RIGHT)
         self.add(rule3)
         self.play(Write(rule3))
+        self.wait()
         self.next_slide()
 
         prob = MathTex("p(\\text{heads})=\\frac{1}{5}", font_size=36).next_to(rule3, DOWN).set_x(0)
         self.add(prob)
         self.play(Write(prob))
+        self.wait()
         self.next_slide()
 
         coin2 = Circle(radius=0.5, color=YELLOW, fill_opacity=1)
@@ -79,11 +91,13 @@ class intro(Slide):
         tails = Text("T", font_size=36, color=BLACK).move_to(coin2.get_center())
         self.add(coin2, tails)
         self.play(Create(coin2))
+        self.wait()
         self.next_slide()
 
         rule4 = Text('Step 4: If the coin lands heads,\nmove to the proposed asteroid.\nKeep track of where you end\nup after each "round"', font_size=24).next_to(coin2, DOWN).to_edge(RIGHT)
         self.add(rule4)
         self.play(Write(rule4))
+        self.wait()
         self.next_slide()
 
         asteroid_visits = Text("Asteroid Visits: [A", font_size=24).to_corner(UL)
@@ -92,6 +106,7 @@ class intro(Slide):
         self.play(Write(asteroid_visits))
         self.add(asteroid_visits2)
         self.play(Write(asteroid_visits2))
+        self.wait()
         self.next_slide()
 
         self.play(Unwrite(rule1), Unwrite(rule2), Unwrite(rule3), Unwrite(rule4), Uncreate(coin2), Unwrite(coinCircle), Unwrite(prob), Unwrite(tails), Unwrite(clockwise), Uncreate(clockwise_arrow), Unwrite(Heads))
@@ -104,6 +119,7 @@ class intro(Slide):
         move_arrow = Arrow([-4,-1, 0], [-3, 1 ,0], color=WHITE)
         self.add(second_proposal, coin, tails, move_arrow)
         self.play(Write(second_proposal), Create(coin), Write(tails), Create(move_arrow))
+        self.wait()
         self.next_slide()
 
         second_coin = Circle(radius=0.5, color=RED, fill_opacity=1).next_to(coin, DOWN)
@@ -111,15 +127,18 @@ class intro(Slide):
         prob = MathTex("p(\\text{heads})=\\frac{1}{3}", font_size=36).next_to(second_coin, RIGHT)
         self.add(second_coin, heads, prob)
         self.play(Create(second_coin), Write(heads), Write(prob))
+        self.wait()
         self.next_slide()
 
         self.play(person.animate.move_to([-3, 2, 0]).scale(0.5), Unwrite(move_arrow))
+        self.wait()
         self.next_slide()
 
         asteroid_visits3 = Text(", C]", font_size=24).next_to(asteroid_visits, RIGHT, buff=0.05)
         self.play(Unwrite(asteroid_visits2))
         self.add(asteroid_visits3)
         self.play(Write(asteroid_visits3))
+        self.wait()
         self.next_slide()
         
         self.play(FadeOut(second_proposal), Unwrite(second_coin), Unwrite(prob), Unwrite(heads), Unwrite(tails), Unwrite(coin), FadeOut(person), FadeOut(asteroids), FadeOut(asteroid_visits), FadeOut(asteroid_visits3))
@@ -359,10 +378,34 @@ class simulation(Slide):
             results = mcmc_sim(current, previous, start=False)
             current = results[0]
             previous = results[1]
-        for i in range(1000):
+        for i in range(1200):
             results = mcmc_sim(current, previous, fast=True)
             current = results[0]
             previous = results[1]
+        self.wait()
+        self.next_slide()
+
+        # remove the everything except the chart)
+        self.remove(*self.mobjects)
+        self.add(chart)
+        self.wait()
+        self.next_slide()
+
+        # show the chart with the "true" values
+        true_chart = BarChart(
+            sizes,
+            y_range=[0, max(sizes), 1],
+            y_length=6,
+            x_length=5,
+            bar_names=letters,
+            y_axis_config={"include_ticks": False,
+                            "font_size": 0.001,}
+        ).to_edge(LEFT).to_edge(UP)
+        chart_subtitle = Text("True Asteroid Sizes", font_size=32).next_to(true_chart, DOWN)
+        self.add(true_chart, chart_subtitle)
+        self.play(Write(chart_subtitle), Create(true_chart))
+        self.wait()
+        self.next_slide()
 
 class explanation(Slide):
     def construct(self):
@@ -400,7 +443,7 @@ class explanation(Slide):
         self.wait()
         self.next_slide()
 
-        crazy_integral = MathTex("\\int e^{-x^2} + e^{-(x-2)^2} + 0.5 e^{-(x+1.5)^2} dx", font_size=36).next_to(crazy_posterior, DOWN)
+        crazy_integral = MathTex("k \\int e^{-x^2} + e^{-(x-2)^2} + 0.5 e^{-(x+1.5)^2} dx", font_size=36).next_to(crazy_posterior, DOWN)
         self.add(crazy_integral)
         self.play(Write(crazy_integral))
         self.wait()
@@ -408,6 +451,27 @@ class explanation(Slide):
 
         self.remove(crazy_posterior, crazy_integral, reason1, reason2, reason3, useful)
         self.play(FadeOut(crazy_posterior), FadeOut(crazy_integral), FadeOut(reason1), FadeOut(reason2), FadeOut(reason3), FadeOut(useful))
+        self.wait()
+        self.next_slide()
+
+        MARC_title = Text("MARC Project", font_size=36).to_edge(UP)
+        self.add(MARC_title)
+        self.play(Write(MARC_title))
+        self.wait()
+        self.next_slide()
+
+        MARC_description = Text("Modeling Bird Occupancy", font_size=36).next_to(MARC_title, DOWN)
+        self.add(MARC_description)
+        self.play(Write(MARC_description))
+        gcki = ImageMobject("gcki.png").scale(0.33).next_to(MARC_description, DOWN).to_edge(LEFT)
+        bbwo = ImageMobject("bbwo.png").scale(0.33).next_to(MARC_description, DOWN).to_edge(RIGHT)
+        self.add(gcki, bbwo)
+        self.play(FadeIn(gcki), FadeIn(bbwo))
+        self.wait()
+        self.next_slide()
+
+        self.remove(MARC_title, MARC_description, gcki, bbwo)
+        self.play(FadeOut(MARC_title), FadeOut(MARC_description), FadeOut(gcki), FadeOut(bbwo))
         self.wait()
         self.next_slide()
 
